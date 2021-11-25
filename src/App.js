44a3a169e92037ab05f1b814f23feb02b24942cd
emma <emma.nouti@gmail.com>
2021-11-25 21:36:47 +0100

    pause pour api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const [ track, setTrack ] = useState(null);
+    const [ lyrics, setLyrics ] = useState(null);
+    
+    useEffect(() => {
+      axios
+        .get('https://random-words-api.vercel.app/word')
+        .then((res) => res.data)
+        .then((data) => data.message.body.track_list)
+        .then((tracklist) => setTrack(tracklist))
+        .catch((err) => console.log(err))
+    }, []);
+    
+    useEffect(() => {
+      if (track) {
+        let a = track.word
+        let link = a.replace(/['"]+/g, '')
+        axios
+          .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${link}`)
+          .then((res) => res.data)
+          .then((data) => data.message.body.lyrics)
+          .then((lyricsArray) => setLyrics(lyricsArray))}
+    }
+    , [track])
+
   return (
     
   );
--- src/components/GiveSentences.js
+++ src/components/GiveSentences.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function GiveSentences({words}) {
+function GiveSentences({words, numbSentence}) {
     let nouns = words.filter((e) => e.meaning.partOfSpeech==="noun")
+    let verbs = words.filter((e) => e.meaning.partOfSpeech==="verb")
+    let adj = words.filter((e) => e.meaning.partOfSpeech==="adj")
+    let adverb = words.filter((e) => e.meaning.partOfSpeech==="adverb")
+    let exclamation = words.filter((e) => e.meaning.partOfSpeech==="exclamation")
+
     const sentencesType = [
         `The ${noun} wants to ${verb} but does he ?`,
         `Why, oh why the ${noun} ${adj}, they couldn't ${verb}.`,
@@ -11,11 +16,13 @@ function GiveSentences({words}) {
         `Once upon a time you ${verb} ${adverb}`,
         `He doesn't ${verb} any ${noun}`,
         `Wherever you ${verb}, Whenever you ${verb}`,
-        `The chance won't come again and don't ${verb} too soon`
+        `The chance won't come again and won't ${verb} too soon`,
+        `One girl, millions ${adj}`
     ]
     function TurnIntoSentences(noun, verb, adj, adverb, exclamation) {
+        let randomSentence = Math.floor(Math.random()*(numbSentence))
 
-    return ""
+    return 
     }
 
     return sentences
