a62a7e15baa9567523379948044e482988907de4
Malik Cherfi <malikcerfi@hotmail.fr>
2021-11-25 22:07:37 +0100

    button pas tout a fait fonctionnel c'est la hess woulah

--- src/pages/TindPages.jsx
+++ src/pages/TindPages.jsx
@@ -4,27 +4,38 @@ const tab = [
   { name: "mama", type: "adjectif" },
   { name: "daz", type: "feezf" },
   { name: "jeanluc", type: "juju" },
+  { name: "pedro", type: "blblba" },
+  { name: "archic", type: "jucezju" },
+  { name: "jeauc", type: "jujccdcu" },
+  { name: "jeanecluc", type: "juju" },
 ];
 const objet = tab.map((e) => e.name);
 const phrase = [];
 
 function TindPages() {
-  const [state, setState] = React.useState("de");
+  
+  const [state, setState] = React.useState("dyd");
 
+  const deleteWord = () => {
+    let index = Math.floor(Math.random() * objet.length);
+    return (objet.splice(index, 1),
+            tab.splice(index, 1), 
+            setState(objet[index])
+     );
+  };
 
-  const randomWordDislike = () => {
-      return setState(objet[Math.floor(Math.random() * objet.length)])
-
-  }
-
-  const randomWord = () => {
-    let i = Math.floor(Math.random() * objet.length);
-    return setState(objet[i]), 
-    phrase.push(tab[i]),
-    tab.shift(tab[i]);
+  const addWord = () => {
+    let index = Math.floor(Math.random() * objet.length);
+    return (
+        setState(objet[index]),
+      phrase.push(tab[index]),
+      objet.splice(index, 1),
+      tab.splice(index, 1)
+  
+    );
   };
   console.log(phrase);
-  console.log(tab)
+  console.log(objet);
 
   return (
     <div>
@@ -32,8 +43,10 @@ function TindPages() {
         <h1>{state}</h1>
       </div>
       <div className="button-container">
-        <button type="button" onClick={randomWordDislike}>Dislike</button>
-        <button type="button" onClick={randomWord}>
+        <button type="button" onClick={deleteWord}>
+          Dislike
+        </button>
+        <button type="button" onClick={addWord}>
           Like
         </button>
       </div>
