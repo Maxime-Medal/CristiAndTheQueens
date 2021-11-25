40de7b7881196eb6d0c0de23b75e8b860040174a
Claire <clairebrunelx@gmail.com>
2021-11-25 22:13:08 +0100

    api call bug

--- src/components/ApiDictonary.jsx
+++ src/components/ApiDictonary.jsx
@@ -2,28 +2,28 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const ApiDictonary = () => {
-  const [track, setTrack] = useState([]);
-  const [lyrics, setLyrics] = useState([]);
+  const [words, setWords] = useState([]);
+  const [wordsDetails, setWordsDetails] = useState([]);
 
   useEffect(() => {
     axios
       .get("https://random-words-api.vercel.app/word")
       .then((res) => res.data)
-      .then((tracklist) => setTrack(tracklist))
-      .then(console.log(track))
+      .then((res) => setWords(res))
       .catch((err) => console.log(err));
   }, []);
 
   useEffect(() => {
-    if (track) {
-      let a = track
-      console.log(a);
+    if (words) {
+      let a = words[0].word
+      let link = a.replace(/'/gi, "");
       axios
-        .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${track.word}`)
+        .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${link}`)
         .then((res) => res.data)
-        .then((lyricsArray) => setLyrics(lyricsArray));
+        .then((res) => setWordsDetails(res))
+        .then(console.log(wordsDetails));
     }
-  }, [track]);
+  }, [words]);
 
   return <div></div>;
 };
