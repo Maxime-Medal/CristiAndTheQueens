ed56791609b347af754870a4fa34e99ff2c5ce48
emma <emma.nouti@gmail.com>
2021-11-26 11:02:05 +0100

    GiveSentences opérationnel ! bam

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import './App.css';
-import Adjectives from './components/Adjectives';
-import Verbs from './components/Verbs';
 import Home from './pages/Home';
+import GiveSentences from './components/GiveSentences.jsx';
 
 const App = () => {
+  let array = ['army',
+  'literature', "kill",
+  "want",
+  "wipe",
+  "integrate",
+  "persist",
+  "view",
+  "fear", "scary",
+  "guiltless",
+  "normal",
+  "trashy",
+  "half",
+  "jolly", 'solidly',
+  'successfully',
+  'upward', 'airport',
+  'tradition',
+  'refrigerator',
+  'comparison',
+  'guitar',
+  'dad',
+  'message',
+  'teaching',
+  'beautifully'];
+  let nb = 5;
   return (
     <div className="App">
-    <Home />
-    <Verbs />
-    <Adjectives />
+      <Home />
+      <GiveSentences words={array} numbSentence={nb} />
     </div>
   );
 };
